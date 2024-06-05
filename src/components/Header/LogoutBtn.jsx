ca3334afcb8afab68f08555b9de34550660623a0
AJSteel01 <abhigyan.209301092@muj.manipal.edu>
2024-06-05 23:14:50 +0530

    building comp

---
+++ src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import {logout} from '../../store/authSlice';
+import authService from '../../backend/conf';
+function LogoutBtn() {
+
+    const dispatch = useDispatch()
+    const logoutHandler =() =>{
+        authService.logout().then(()=>{
+            dispatch(logout())
+        })
+    }
+  return (
+    <button class="py-2.5 px-6 rounded-lg text-sm font-medium text-white bg-teal-600">Logout</button>
+  )
+}
+
+export default LogoutBtn
