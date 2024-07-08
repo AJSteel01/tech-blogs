3a9b07fb49a12c1af57dcc53fea4841101a7c7c4
AJSteel01 <abhigyan.209301092@muj.manipal.edu>
2024-07-08 22:22:49 +0530

    Building Pages

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,10 @@ import Logo from "./Logo";
 import Button from "../components/Button"
 import Loader from "./Loader"
 import RTE from "./RTE";
-
+import Signup from './Signup'
+import Login from './Login'
+import PostForm from './post-form/PostForm'
+import PostCard from './PostCard'
 export{
     Header,
     Footer,
@@ -15,5 +18,9 @@ export{
     Logo,
     Button,
     Loader,
-    RTE
+    RTE,
+    Signup,
+    Login,
+    PostCard,
+    PostForm
 }
